2bbab76bb9c52012fa8d89683644aabed15bb74e
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-08-22 14:32:06 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+package guvi;
+
+public class Factorial
+{
+   public static void main(String args[])
+   {  
+     int i,fact=1;  
+    int number=10;    
+     for(i=1;i<=number;i++)
+     {    
+         fact=fact*i;    
+     }    
+     	System.out.println("Factorial of "+number+" is: "+fact);    
+    }  
+   }  
